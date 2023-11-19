2ceb8877acafb5da700129a3c025948be8d8326c
1072030 <229jdu@gmail.com>
2023-11-19 06:39:33 +0000

    update

--- app/services/statistics.py
+++ app/services/statistics.py
@@ -7,13 +7,16 @@ from app.core.database import (
     PredictResult,
     transaction
 )
+from datetime import datetime, timedelta
+import pandas as pd
+import numpy as np
 
 
-async def GetPredictResult(project_name:Optional[str] = None, device_name: Optional[str] = None):
+async def GetPredictResult(project_name: Optional[str] = None, device_name: Optional[str] = None):
     # if project_name is None:
     project_id_list = []
     check_project_name = project_name.split(',')
-    single=None
+    single = None
     if len(check_project_name) == 2:
         project_id_list = list(map(int, project_name.split(',')))
     else:
@@ -23,15 +26,15 @@ async def GetPredictResult(project_name:Optional[str] = None, device_name: Optio
             print('not single')
 
     if len(project_id_list) == 2:
-        data = await Project.objects.filter(id__in=project_id_list).select_related(['devices']).all()
+        data = await Project.objects.filter(id__in=project_id_list).select_related(['devices', 'devices__events']).all()
     else:
         if single is None:
             if device_name is None:
-                data = await Project.objects.filter(name=project_name).select_related(['devices']).all()
+                data = await Project.objects.filter(name=project_name).select_related(['devices', 'devices__events']).all()
             else:
-                data = await Project.objects.filter(name=project_name).select_related(['devices']).filter(devices__name=device_name).all()
+                data = await Project.objects.filter(name=project_name).select_related(['devices', 'devices__events']).filter(devices__name=device_name).all()
         else:
-            data = await Project.objects.filter(id=single).select_related(['devices']).all()
+            data = await Project.objects.filter(id=single).select_related(['devices', 'devices__events']).all()
 
     # devices = data[0].devices
     project_device = [project.devices for project in data]
@@ -39,18 +42,27 @@ async def GetPredictResult(project_name:Optional[str] = None, device_name: Optio
     for i in project_device:
         for j in i:
             devices.append(j)
-    devices_id = [dvs.id for dvs in devices]
-    predictResult = await PredictResult.objects.filter(device__in=devices_id).order_by('pred_date').all()
-    unique_events = set([result.event.id for result in predictResult])
+    # devices_id = [dvs.id for dvs in devices]
     getAllFirstResultData = []
-    for event in unique_events:
-        firstResultData_week = await PredictResult.objects.filter(device__in=devices_id, event=event,pred_type=1).select_related(['event']).order_by('-pred_date').first()
-        firstResultData_day = await PredictResult.objects.filter(device__in=devices_id, event=event,pred_type=0).select_related(['event']).order_by('-pred_date').first()
-        getAllFirstResultData.append(firstResultData_week)
-        getAllFirstResultData.append(firstResultData_day)
+    for dvs in devices:
+
+        events = dvs.events
+        for event in events:
+            checkPredEvent = await PredictResult.objects.filter(event=event.id).order_by('-pred_date').limit(1).get_or_none()
+
+            # check
+            if checkPredEvent is None:
+                continue
+
+            firstResultData_week = await PredictResult.objects.filter(device=dvs.id, event=event, pred_type=1).select_related(['event']).order_by('-pred_date').limit(1).get_or_none()
+            firstResultData_day = await PredictResult.objects.filter(device=dvs.id, event=event, pred_type=0).select_related(['event']).order_by('-pred_date').limit(1).get_or_none()
+            getAllFirstResultData.append(firstResultData_week)
+            getAllFirstResultData.append(firstResultData_day)
 
     formatData = {}
     for result in getAllFirstResultData:
+        if result is None:
+            continue
         for i in devices:
             if result.device.id == i.id:
                 dvs_project_name = " ".join(
@@ -64,7 +76,7 @@ async def GetPredictResult(project_name:Optional[str] = None, device_name: Optio
                     pred_type = "週預測"
                 else:
                     pred_type = "日預測"
-                
+
                 formatData[dvs_project_name][dvs_name].append({
                     'id': result.id,
                     'name': result.event.name,
@@ -73,4 +85,97 @@ async def GetPredictResult(project_name:Optional[str] = None, device_name: Optio
                     'frequency': pred_type,
                     'happenLastTime': True
                 })
+    return formatData
+
+
+async def GetPredictCompareSearch(project_name: List, device_name: str,select_type:str, line: int, start_time: datetime, end_time: datetime):
+    formatData = []
+    for project in project_name:
+        if device_name is None:
+            project_devices = await Project.objects.select_related(["devices", "devices__events"]).filter(name=project).all()
+
+        else:
+            if line is None:
+                project_devices = await Project.objects.select_related(["devices", "devices__events"]).filter(name=project, devices__name=device_name).all()
+            else:
+                project_devices = await Project.objects.select_related(["devices", "devices__events"]).filter(name=project, devices__name=device_name, devices__line=line).all()
+
+        devices = project_devices[0].devices
+        # dr = pd.date_range(
+        # first_data_date, datetime.datetime.now().date(), freq='2M').astype(str)
+        dr_day = pd.date_range(start_time, end_time).astype(str)
+        dr_week = pd.date_range(start_time, end_time,freq='7D').astype(str)
+        if select_type == "day":
+            for date in dr_day:
+                actual_check = []
+                predict_check = []
+                for dvs in devices:
+                    events = dvs.events
+                    for event in events:
+                        checkPredEvent = await PredictResult.objects.filter(event=event.id,pred_type=0).order_by('-pred_date').limit(1).get_or_none()
+
+                        # check
+                        if checkPredEvent is None:
+                            continue
+                        
+                        data = await PredictResult.objects.filter(event=event.id,pred_date=date,pred_type=0).select_related("device").order_by('-pred_date').limit(1).get_or_none()
+                        if data is None:
+                            continue
+                        if data.last_happened_check is True:
+                            if data.pred == '0':
+                                predict_check.append(0)
+                            else:
+                                predict_check.append(1)
+                            if data.last_happened is None:
+                                actual_check.append(0)
+                            else:
+                                actual_check.append(1)
+                    accuracy = (np.array(actual_check) == np.array(predict_check)).mean()
+                    if len(predict_check) != 0 and len(actual_check) != 0 :
+                        formatData.append({
+                            "id":data.id,
+                            "projectName":project,
+                            "deviceName":dvs.name,
+                            "line":dvs.line,
+                            "date":date,
+                            "accuracyDate": accuracy,
+                            "trend":"查看"
+                        })
+
+        else:
+            for date in dr_week:
+                actual_check = []
+                predict_check = []
+                for dvs in devices:
+                    events = dvs.events
+                    for event in events:
+                        checkPredEvent = await PredictResult.objects.filter(event=event.id,pred_type=1).order_by('-pred_date').limit(1).get_or_none()
+
+                        # check
+                        if checkPredEvent is None:
+                            continue
+                        
+                        data = await PredictResult.objects.filter(event=event.id,pred_date=date,pred_type=1).select_related("device").order_by('-pred_date').limit(1).get_or_none()
+                        if data is None:
+                            raise HTTPException(status_code=400,detail=f"can not find data")
+                        if data.last_happened_check is True:
+                            if data.pred == '0':
+                                predict_check.append(0)
+                            else:
+                                predict_check.append(1)
+                            if data.last_happened is None:
+                                actual_check.append(0)
+                            else:
+                                actual_check.append(1)
+                    accuracy = (np.array(actual_check) == np.array(predict_check)).mean()
+                    if len(predict_check) != 0 and len(actual_check) != 0 :
+                        formatData.append({
+                            "id":data.id,
+                            "projectName":project,
+                            "deviceName":dvs.name,
+                            "line":dvs.line,
+                            "date":date,
+                            "accuracyWeek": accuracy,
+                            "trend":"查看"
+                        })
     return formatData
